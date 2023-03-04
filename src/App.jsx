9b4f0893a91d7c633fd897013d10b30760d89239
Franco Gallo <gallofrancogabriel@gmail.com>
2023-03-04 12:28:24 +0100

    Portfolio bases

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import NavBar from "./components/NavBar";
+
+function App() {
+  return (
+    <header id="header">
+      <NavBar/>
+    </header>
+  );
+}
+
+export default App;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <nav>
+        <ul>
+            <li><a href='header'>Inicio</a></li>
+            <li><a href='header'>Proyectos</a></li>
+            <li><a href='header'>Contacto</a></li>
+        </ul>
+    </nav>
+  )
+}
+
+export default NavBar
